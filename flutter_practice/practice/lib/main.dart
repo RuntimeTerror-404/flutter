b951822356786a20a6a18db642cc44ab67b1eb25
Mohit Parashar <mohit1672002@gmail.com>
2021-01-16 15:53:13 +0530

    CH2 completed

--- flutter_practice/practice/lib/main.dart
+++ flutter_practice/practice/lib/main.dart
@@ -1,3 +1,6 @@
+// ignore: avoid_web_libraries_in_flutter
+// import 'dart:html';
+// ignore: unused_import
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
@@ -15,10 +18,38 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Center(child: Text("Hi Flutter")),
-        backgroundColor: Colors.black26,
+        backgroundColor: Colors.purple,
       ),
-      body: Container(
-        child: Center(child: Text("App developed by Mohit")),
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(10),
+          alignment: Alignment.center,
+          // color: Colors.purple[200],
+          width: 200,
+          height: 150,
+          child: Text(
+            "This is a Box",
+            style: TextStyle(
+                color: Colors.black, fontSize: 25, fontWeight: FontWeight.bold,
+                textBaseline: TextBaseline.alphabetic),
+          ),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+              // shape: BoxShape.circle,
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.grey[600],
+                    blurRadius: 10,
+                    offset: Offset(2.0, 5.0))
+              ],
+              color: Colors.teal,
+              gradient: LinearGradient(colors: [
+                Colors.redAccent,
+                // Colors.lightBlue[300],
+                Colors.greenAccent,
+                Colors.pink[400]
+              ])),
+        ),
       ),
     );
   }
